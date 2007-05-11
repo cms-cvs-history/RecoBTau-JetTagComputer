d43c7ff949ed07e31bb232adf189d1c55646b330
saout <sha1-a9a1d87b03630df246ec7c3919d4d79e9a149492@cern.ch>
2007-05-11 14:16:38 +0000

    make setEventSetup callable from const and m_computer a mutable cache, add working test

--- test/testMakeMVAComputerCondDB.cc
+++ test/testMakeMVAComputerCondDB.cc
@@ -21,9 +21,9 @@
 
 using namespace PhysicsTools::Calibration;
 
-class GenericMVAJetTagMakeDiscriminatorCondDB : public edm::EDAnalyzer {
+class testMakeMVAComputerCondDB : public edm::EDAnalyzer {
     public:
-	explicit GenericMVAJetTagMakeDiscriminatorCondDB(const edm::ParameterSet &params);
+	explicit testMakeMVAComputerCondDB(const edm::ParameterSet &params);
 
 	virtual void analyze(const edm::Event& iEvent,
 	                     const edm::EventSetup& iSetup);
@@ -34,50 +34,49 @@ class GenericMVAJetTagMakeDiscriminatorCondDB : public edm::EDAnalyzer {
 	std::string	record;
 };
 
-GenericMVAJetTagMakeDiscriminatorCondDB::GenericMVAJetTagMakeDiscriminatorCondDB(
+testMakeMVAComputerCondDB::testMakeMVAComputerCondDB(
 					const edm::ParameterSet &params) :
 	record(params.getUntrackedParameter<std::string>("record"))
 {
 }
 
-void GenericMVAJetTagMakeDiscriminatorCondDB::analyze(const edm::Event& iEvent,
+void testMakeMVAComputerCondDB::analyze(const edm::Event& iEvent,
                                       const edm::EventSetup& iSetup)
 {
 }
 
-void GenericMVAJetTagMakeDiscriminatorCondDB::endJob()
+void testMakeMVAComputerCondDB::endJob()
 {
 // set up some dummy calibration by hand for testing
 
 	MVAComputerContainer *container = new MVAComputerContainer;
-	MVAComputer *computer = &container->add("TestCalibration");
+	MVAComputer *computer = &container->add("ImpactParameter");
 
 // vars
 
 	Variable var;
-	var.name = "test";
+	var.name = "trackSip2d";
 	computer->inputSet.push_back(var);
 
-	var.name = "normal";
-	computer->inputSet.push_back(var);
-
-	var.name = "toast";
+	var.name = "trackSip3d";
 	computer->inputSet.push_back(var);
 
 // normalize
 
 	ProcNormalize *norm = new ProcNormalize();
 
-	PhysicsTools::BitSet testSet(3);
+	PhysicsTools::BitSet testSet(2);
 	testSet[0] = testSet[1] = true;
 	norm->inputVars = convert(testSet);
 
 	PDF pdf;
+	pdf.distr.push_back(0.8);
+	pdf.distr.push_back(1.7);
+	pdf.distr.push_back(1.4);
 	pdf.distr.push_back(1.0);
-	pdf.distr.push_back(1.5);
-	pdf.distr.push_back(1.0);
-	pdf.range.first = 4.0;
-	pdf.range.second = 5.5;
+	pdf.distr.push_back(0.8);
+	pdf.range.first = -0.05;
+	pdf.range.second = 0.2;
 	norm->distr.push_back(pdf);
 	norm->distr.push_back(pdf);
 
@@ -87,30 +86,23 @@ void GenericMVAJetTagMakeDiscriminatorCondDB::endJob()
 
 	ProcLikelihood *lkh = new ProcLikelihood();
 
-	testSet = PhysicsTools::BitSet(5);
-	testSet[2] = true;
+	testSet = PhysicsTools::BitSet(4);
+	testSet[2] = testSet[3] = true;
 	lkh->inputVars = convert(testSet);
 
-	pdf.distr.push_back(1.0);
-	pdf.distr.push_back(1.5);
-	pdf.distr.push_back(1.0);
+	pdf.distr.push_back(0.1);
+	pdf.distr.push_back(0.5);
+	pdf.distr.push_back(0.9);
 	pdf.range.first = 0.0;
 	pdf.range.second = 1.0;
 	ProcLikelihood::SigBkg sigBkg;
 	sigBkg.signal = pdf;
-	pdf.distr.push_back(1.5);
-	pdf.distr.push_back(1.0);
-	pdf.distr.push_back(1.7);
+	pdf.distr.clear();
+	pdf.distr.push_back(0.9);
+	pdf.distr.push_back(0.5);
+	pdf.distr.push_back(0.1);
 	sigBkg.background = pdf;
 	lkh->pdfs.push_back(sigBkg);
-
-	computer->addProcessor(lkh);
-
-// likelihood 2
-
-	testSet = PhysicsTools::BitSet(6);
-	testSet[2] = testSet[3] = true;
-	lkh->inputVars = convert(testSet);
 	lkh->pdfs.push_back(sigBkg);
 
 	computer->addProcessor(lkh);
@@ -119,60 +111,27 @@ void GenericMVAJetTagMakeDiscriminatorCondDB::endJob()
 
 	ProcOptional *opt = new ProcOptional();
 
-	testSet = PhysicsTools::BitSet(7);
-	testSet[5] = testSet[6] = true;
+	testSet = PhysicsTools::BitSet(5);
+	testSet[4] = true;
 	opt->inputVars = convert(testSet);
 
-	opt->neutralPos.push_back(0.6);
-	opt->neutralPos.push_back(0.7);
+	opt->neutralPos.push_back(-1.0);
 
 	computer->addProcessor(opt);
 
-// PCA
-
-	ProcMatrix *pca = new ProcMatrix();
-
-	testSet = PhysicsTools::BitSet(9);
-	testSet[4] = testSet[7] = testSet[8] = true;
-	pca->inputVars = convert(testSet);
-
-	pca->matrix.rows = 2;
-	pca->matrix.columns = 3;
-	double elements[] = { 0.2, 0.3, 0.4, 0.8, 0.7, 0.6 };
-	std::copy(elements, elements + sizeof elements / sizeof elements[0],
-	          std::back_inserter(pca->matrix.elements));
-
-	computer->addProcessor(pca);
-
-// linear
-
-	ProcLinear *lin = new ProcLinear();
-
-	testSet = PhysicsTools::BitSet(11);
-	testSet[9] = testSet[10] = true;
-	lin->inputVars = convert(testSet);
-
-	lin->coeffs.push_back(0.3);
-	lin->coeffs.push_back(0.7);
-	lin->offset = 0.0;
-
-	computer->addProcessor(lin);
-
 // output
 
-	computer->output = 11;
+	computer->output = 5;
 
 // test computer
 
 	PhysicsTools::MVAComputer comp(computer);
 
 	PhysicsTools::Variable::Value values[] = {
-		PhysicsTools::Variable::Value("toast", 4.4),
-		PhysicsTools::Variable::Value("toast", 4.5),
-		PhysicsTools::Variable::Value("test", 4.6),
-		PhysicsTools::Variable::Value("toast", 4.7),
-		PhysicsTools::Variable::Value("test", 4.8),
-		PhysicsTools::Variable::Value("normal", 4.9)
+		PhysicsTools::Variable::Value("trackSip2d", 0.14),
+		PhysicsTools::Variable::Value("trackSip3d", 0.12),
+		PhysicsTools::Variable::Value("trackSip2d", 0.02),
+		PhysicsTools::Variable::Value("trackSip3d", -0.03),
 	};
 
 	std::cout << comp.eval(values,
@@ -194,4 +153,4 @@ void GenericMVAJetTagMakeDiscriminatorCondDB::endJob()
 
 // define this as a plug-in
 DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(GenericMVAJetTagMakeDiscriminatorCondDB);
+DEFINE_ANOTHER_FWK_MODULE(testMakeMVAComputerCondDB);
