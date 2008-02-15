44fc04bfbe5059b8814d6a57dc3e61b4ab3e9694
saout <sha1-a9a1d87b03630df246ec7c3919d4d79e9a149492@cern.ch>
2008-02-15 17:23:55 +0000

    Attempt to check in backport, part 1.

---
+++ plugins/JetTagProducer.cc
@@ -0,0 +1,182 @@
+// -*- C++ -*-
+//
+// Package:    JetTagProducer
+// Class:      JetTagProducer
+// 
+/**\class JetTagProducer JetTagProducer.cc RecoBTag/JetTagProducer/src/JetTagProducer.cc
+
+ Description: Uses a JetTagComputer to produce JetTags from TagInfos
+
+ Implementation:
+     <Notes on implementation>
+*/
+//
+// Original Author:  Andrea Rizzi
+//         Created:  Thu Apr  6 09:56:23 CEST 2006
+// $Id: JetTagProducer.cc,v 1.7 2007/11/23 16:07:50 saout Exp $
+//
+//
+
+
+// system include files
+#include <functional>
+#include <memory>
+#include <string>
+#include <vector>
+#include <map>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/ParameterSet/interface/InputTag.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Utilities/interface/Exception.h"
+
+#include "DataFormats/Common/interface/View.h"
+#include "DataFormats/Common/interface/RefToBase.h"
+#include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/BTauReco/interface/BaseTagInfo.h"
+#include "DataFormats/BTauReco/interface/JetTag.h"
+
+#include "RecoBTau/JetTagComputer/interface/JetTagComputer.h"
+#include "RecoBTau/JetTagComputer/interface/JetTagComputerRecord.h"
+
+#include "JetTagProducer.h"
+
+using namespace std;
+using namespace reco;
+using namespace edm;
+
+//
+// constructors and destructor
+//
+JetTagProducer::JetTagProducer(const ParameterSet& iConfig) :
+  m_jetTagComputer(iConfig.getParameter<string>("jetTagComputer"))
+{
+  vector<string> inputTags = iConfig.getParameterNamesForType<InputTag>();
+
+  for(vector<string>::const_iterator iter = inputTags.begin();
+      iter != inputTags.end(); iter++) {
+    InputTag inputTag = iConfig.getParameter<InputTag>(*iter);
+    m_tagInfoLabels[*iter] = inputTag;
+  }
+
+  produces<JetTagCollection>();
+}
+
+JetTagProducer::~JetTagProducer()
+{
+}
+
+//
+// member functions
+//
+// ------------ method called once each job just before starting event loop  ------------
+void
+JetTagProducer::beginJob(const edm::EventSetup& iSetup) {
+  edm::ESHandle<JetTagComputer> computer;
+  iSetup.get<JetTagComputerRecord>().get( m_jetTagComputer, computer );
+  m_computer = computer.product();
+  m_computer->setEventSetup(iSetup);
+
+  // finalize the JetTagProducer <-> JetTagComputer glue setup
+  vector<string> inputLabels(m_computer->getInputLabels());
+
+  // backward compatible case, use default tagInfo
+  if (inputLabels.empty())
+    inputLabels.push_back("tagInfo");
+
+  // collect all TagInfos from the ParameterSet that the JetTagComputer wants
+  for(vector<string>::const_iterator iter = inputLabels.begin();
+      iter != inputLabels.end(); iter++) {
+    map<string, InputTag>::const_iterator pos = m_tagInfoLabels.find(*iter);
+    if (pos == m_tagInfoLabels.end())
+      throw cms::Exception("InputTagMissing") << "JetTagProducer is missing "
+      			"a TagInfo InputTag \"" << *iter << "\"" << endl;
+
+    m_tagInfos.push_back(pos->second);
+  }
+}
+
+// map helper - for some reason RefToBase lacks operator < (...)
+namespace {
+  struct JetRefCompare :
+       public binary_function<RefToBase<Jet>, RefToBase<Jet>, bool> {
+    inline bool operator () (const RefToBase<Jet> &j1,
+                             const RefToBase<Jet> &j2) const
+    { return j1.id() < j2.id() || (j1.id() == j2.id() && j1.key() < j2.key()); }
+  };
+}
+
+// ------------ method called to produce the data  ------------
+void
+JetTagProducer::produce(Event& iEvent, const EventSetup& iSetup)
+{
+  m_computer->setEventSetup(iSetup);
+
+  // now comes the tricky part:
+  // we need to collect all requested TagInfos belonging to the same jet
+
+  // FIXME: note that we are storing the index of the View here.
+  // If we drop the RefToBase in the JetTag (i.e. JetToFloatAssociation)
+  // it is simpler to just map to the C++ pointers directly here.
+  typedef std::vector<int> TagInfoRefs;
+  typedef RefToBase<Jet> JetRef;
+  typedef map<JetRef, TagInfoRefs, JetRefCompare> JetToTagInfoMap;
+
+  JetToTagInfoMap jetToTagInfos;
+
+  // retrieve all requested TagInfos
+  vector< Handle< View<BaseTagInfo> > > tagInfoHandles(m_tagInfos.size());
+  unsigned int nTagInfos = m_tagInfos.size();
+  for(unsigned int i = 0; i < nTagInfos; i++) {
+    Handle< View<BaseTagInfo> > &tagInfoHandle = tagInfoHandles[i];
+    iEvent.getByLabel(m_tagInfos[i], tagInfoHandle);
+
+    int cc = 0;
+    for(View<BaseTagInfo>::const_iterator iter = tagInfoHandle->begin();
+        iter != tagInfoHandle->end(); iter++, cc++) {
+      TagInfoRefs &tagInfos = jetToTagInfos[iter->jet()];
+      if (tagInfos.empty())
+        tagInfos.resize(nTagInfos, -1);
+
+      tagInfos[i] = cc;
+    }
+  }
+
+  // now loop over the map and compute all JetTags
+  std::auto_ptr<JetTagCollection> jetTagCollection(new JetTagCollection());
+
+  for(JetToTagInfoMap::const_iterator iter = jetToTagInfos.begin();
+      iter != jetToTagInfos.end(); iter++) {
+    const TagInfoRefs &refs = iter->second;
+
+    // this is unnecessary if RefToBase to BaseTagInfo dropped, see above
+    vector<const BaseTagInfo*> tagInfos(nTagInfos);
+    RefToBase<BaseTagInfo> tagInfoRef;
+    for(unsigned int i = 0; i < nTagInfos; i++) {
+      if (refs[i] < 0)
+        continue;
+
+      tagInfoRef = tagInfoHandles[i]->refAt(refs[i]);
+      tagInfos[i] = tagInfoRef.get();
+    }
+
+    JetTagComputer::TagInfoHelper helper(tagInfos);
+    float discriminator = (*m_computer)(helper);
+
+    JetTag jetTag(discriminator);
+    // set some (the last valid) RefToBase<BaseTagInfo> until it is dropped
+    jetTag.setTagInfo(tagInfoRef);
+
+    jetTagCollection->push_back(jetTag);
+  }
+
+  iEvent.put(jetTagCollection);
+}
+
+// define it as plugin
+#include "FWCore/Framework/interface/MakerMacros.h"
+DEFINE_FWK_MODULE(JetTagProducer);
